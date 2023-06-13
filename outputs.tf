b3e44eaeb45b55eb7a904585e4959d2181bc614d
Ryan Faircloth <ryan@dss-i.com>
2023-06-13 08:09:05 -0400

    feat: initial release

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "name" {
+  value = azurerm_eventhub_consumer_group.cg.name
+}
+output "id" {
+  value = azurerm_eventhub_consumer_group.cg.id
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+ variable "name" {
+  type = string
+  description = "(optional) describe your variable"
+ }
+ variable "namespace_name" {
+  type = string
+  description = "(optional) describe your variable"
+ }
+ variable "eventhub_name" {
+  type = string
+  description = "(optional) describe your variable"
+ }
+ variable "resource_group_name" {
+  type = string
+  description = "(optional) describe your variable"
+ }

d09e95a0d0ad948b43898dad2f6218c53d9330f0
Ryan Faircloth <ryan@dss-i.com>
2023-08-26 17:01:22 -0400

    fix: add creation of connection string

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,6 @@ output "id" {
   value = azurerm_eventhub_consumer_group.cg.id
 }
 output "connection_string" {
+  sensitive = true
   value = azurerm_eventhub_authorization_rule.cg.primary_connection_string
 }
